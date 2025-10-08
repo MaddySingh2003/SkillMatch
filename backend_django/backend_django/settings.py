34bebc5cb9a83593a7c7c84394bdcf4fb58eb55b
maddysingh2003 <miansuryavanshi27@gmail.com>
2025-10-08 14:27:04 +0530

    render allow

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4+fepaz=i=fb#(rl61+fq4bs=*&y^cj&f&@7b3ytt=#zdoh+qd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
