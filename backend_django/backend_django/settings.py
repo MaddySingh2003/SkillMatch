5bde7d8543c351c3090c72c489550c7639f2d26e
maddysingh2003 <miansuryavanshi27@gmail.com>
2025-10-09 14:18:56 +0530

    bebug ture

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4+fepaz=i=fb#(rl61+fq4bs=*&y^cj&f&@7b3ytt=#zdoh+qd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,9 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# If you're using collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# If your theme app has its own static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
